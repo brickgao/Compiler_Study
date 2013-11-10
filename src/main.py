54c45d0a8493b1d2637b1bc0c2770b0e5948178f
Brickgao <brickgao@gmail.com>
2013-11-10 15:41:03 +0800

    fix when use quote method in wrong way

--- src/main.py
+++ src/main.py
@@ -1,37 +1,60 @@
 # -*- coding: UTF-8 -*-
-def dfs(s):
+import gl
+
+def dfs(s, bpos):
     ret = []
     l = -1
     r = len(s)
-    for i in range(0, len(s)):
-        if(s[i] == '('):
-            l = i
-    for i in range(len(s) - 1, -1, -1):
-        if(s[i] == ')'):
-            r = i
-    if l != 0 and s[l - 1] == '\'':
-        vtmp = []
-        stmp = ''
-        for i in range(l + 1, r):
-            stmp += s[i]
-        vtmp = stmp.split(' ')
-        while '' in vtmp:
-            vtmp.remove('')
-        ret = vtmp
+    if '(' in s:
+        l = s.index('(')
+    if ')' in s:
+        r = len(s) - s[-1].index(')') - 1
+    if l == -1 and r == len(s):
+        if s[0] == '\'':
+            startpos = -1
+            for i in range(len(s) - 1, 0, -1):
+                if(s[i] != ' '):
+                    startpos = i
+                    break
+            stmp = s[1:startpos + 1]
+            if startpos == -1 or ' ' in stmp:
+                gl.flag = False
+                if startpos == -1:
+                    gl.pos = 0
+                else:
+                    for i in range(startpos, 0, -1):
+                        if s[i] == ' ':
+                            gl.pos = i + 1 + bpos
+                            break
+            ret.append(stmp)
+        else:
+            gl.flag = False
+            gl.pos = bpos
+    else:
+        if l != 0 and s[l - 1] == '\'':
+            stmp = s[l + 1, r]
+            vtmp = stmp.split(' ')
+            while '' in vtmp:
+                vtmp.remove('')
+            ret = vtmp
     return ret
 
 def run():
     print 'This is a simple parser for lisp.'
     print 'Type "copyright" for more information.'
     while True:
+        gl.flag = True
         print '>>',
         s = raw_input()
         if s == 'copyright':
             print 'Author: Brickgao'
             print 'You can contact me at brickgao#gmail.com'
         else:
-            ans = dfs(s)
-            print ans
+            ans = dfs(s, 0)
+            if gl.flag:
+                print ans
+            else:
+                print 'There is something wrong at', gl.pos, ', please check it'
     
 if __name__ == '__main__':
     run()
